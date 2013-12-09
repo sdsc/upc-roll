0f81fa1f5752a623bfc870f4ada43d9c728eb9c6
Trevor Cooper <tcooper@sdsc.edu>
2013-12-09 08:50:18 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/upc/version.mk
+++ src/upc/version.mk
@@ -1,3 +1,19 @@
-NAME	= upc_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION	= 2.16.0
-RELEASE	= 0
+NAME               = upc_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 2.16.0
+RELEASE            = 0
+PKGROOT            = /opt/upc
+
+SRC_SUBDIR         = upc
+
+SOURCE_NAME        = upc
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+UPC_TRANS_NAME     = upc_translator
+UPC_TRANS_PKG      = $(UPC_TRANS_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+UPC_TRANS_DIR      = $(UPC_TRANS_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG) $(UPC_TRANS_PKG)
+
