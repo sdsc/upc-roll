b3e433cd03c0c70e8ee8d5aa128d384be79bbd3e
Jim Hayes <jhayes@sdsc.edu>
2014-10-01 19:56:22 -0700

    Use canonical modulefile/roll-test build files.

--- src/roll-test/version.mk
+++ src/roll-test/version.mk
@@ -1,5 +1,6 @@
 NAME       = upc-roll-test
 VERSION    = 1
 RELEASE    = 2
+PKGROOT    = /root/rolltests
 
 RPM.EXTRAS = AutoReq:No
--- src/upc-modules/version.mk
+++ src/upc-modules/version.mk
@@ -1,8 +1,11 @@
-NAME        = upc-modules
-RELEASE     = 1
-PKGROOT     = /opt/modulefiles/compilers/upc
+PACKAGE     = upc
+CATEGORY    = compilers
 
-VERSION_SRC = $(REDHAT.ROOT)/src/upc/version.mk
+NAME        = $(PACKAGE)-modules
+RELEASE     = 2
+PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
+
+VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
